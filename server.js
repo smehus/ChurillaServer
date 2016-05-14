9663492ab4e56f84ee878a27f9a3edf0c5147b53
Scott Mehus <mehus.scott@gmail.com>
2016-05-14 10:41:45 -0400

    bug fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-var recipes = require('.routes/recipes')
+var recipes = require('./routes/recipes')
 
 app.listen(port, function() {
 	console.log('Start server on port' + port)
