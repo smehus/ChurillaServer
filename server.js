833b3a228786d27618104cf9826aae671dbe9063
Scott Mehus <mehus.scott@gmail.com>
2016-05-14 09:57:24 -0400

    Added heroku port

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
 
-app.listen(3000)
+app.listen(port, function() {
+	console.log('Start server on port' + port)
+})
 
 app.get('/get', function(req, res) {
 	res.send([{name:'wine1'}, {name:'wine2'}]);
